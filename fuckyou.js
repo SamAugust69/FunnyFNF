73226b2a8d1e080168d3c18a388463e5f80eff9c
SamAugust69 <sammonsonis@gmail.com>
2021-05-10 21:27:51 -0400

    ayo

--- fuckyou.js
+++ fuckyou.js
@@ -1,27 +1,13 @@
-import * as fs from 'fs';
+function click() {
+    var dropdown = document.getElementById("update-dropdown");
+    var text = document.getElementById("funnytext");
+    dropdown.height(text.outerHeight(true));
 
-var table = document.getElementById("modTable")
-_readFile('./mods.json', 'utf8', (jsonString) => {
-    return JSON.parse(jsonString);
-})
-
-function buildTable(data) {
-    for (var i = 0; i < data.length; i++) {
-        var row = "<tr><td>${data[i].mod}</td></tr>"
-        table.innerHTML += row
+    if (dropdown.hasClass("open")) {
+        dropdown.removeClass("open");
+        dropdown.height(0);
+    } else {
+        dropdown.addClass("open");
+        dropdown.height(text.outerHeight(true))
     }
 }
-buildTable(readFile("mods.json"))
-$(document).ready(function() {
-    $.getJSON("mods.json", function(data) {
-        var mod_data = '';
-        $.each(data, function(key, value) {
-            console.log(value.modLink)
-            mod_data += "<tr>";
-            mod_data += '<td style="color: rgb(212, 225, 212);">' + value.modLink + "</td>";
-            mod_data += '<td style="color: rgb(212, 225, 212);">' + value.mod + "</td>";
-            mod_data += "</tr>"
-        });
-        $("#table").append(mod_data);
-    });
-});
--- testing_folder/funny.js
+++
@@ -1,1 +0,0 @@
-var funnybtn = document.querySelector(".testbtn")
