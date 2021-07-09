a2fc9b99e7ad07459c4fbdb2fc8e32abd7b0176d
SamAugust69 <sammonsonis@gmail.com>
2021-07-09 03:34:20 -0400

    coool

--- downloadBtn.js
+++ downloadBtn.js
@@ -1,29 +1,3 @@
-function EngClick() {
-    var van100Btn = document.getElementById("100x100_btn")
-    var van100Page = document.getElementById("100x100_page")
-
-    var engBtn = document.getElementById("enigmatica_btn")
-    var engPage = document.getElementById("eng_page")
-
-    van100Page.style.display = "none";
-    engPage.style.display = "block";
-    engBtn.className = "page_sel_button-selected";
-    van100Btn.className = "page_sel_button";
-}
-
-function van100Click() {
-    var van100Btn = document.getElementById("100x100_btn")
-    var van100Page = document.getElementById("100x100_page")
-
-    var engBtn = document.getElementById("enigmatica_btn")
-    var engPage = document.getElementById("eng_page")
-
-    van100Page.style.display = "block";
-    engPage.style.display = "none";
-    engBtn.className = "page_sel_button";
-    van100Btn.className = "page_sel_button-selected";
-}
-
 var engDropdownOpen = false
 
 function engDropdown() {
@@ -34,8 +8,27 @@ function engDropdown() {
         dropdown[0].style.height = "0px"
         engDropdownOpen = false;
     } else {
-        dropdown[0].style.height = "68px";
+        dropdown[0].style.height = "90px";
         dropdown[0].style.marginBottom = "16px";
         engDropdownOpen = true;
+    }
+}
+function pagePress(btn_id) {
+    var van100Btn = document.getElementById("100x100_btn")
+    var van100Page = document.getElementById("100x100_page")
+
+    var engBtn = document.getElementById("enigmatica_btn")
+    var engPage = document.getElementById("eng_page")
+
+    if (btn_id == "100x100_btn") {
+        van100Page.style.display = "block";
+        engPage.style.display = "none";
+        engBtn.className = "page_sel_button";
+        van100Btn.className = "page_sel_button-selected";
+    } else {
+        van100Page.style.display = "none";
+        engPage.style.display = "block";
+        engBtn.className = "page_sel_button-selected";
+        van100Btn.className = "page_sel_button";
     }
 }
